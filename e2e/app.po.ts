abeeb856605b0a8b8b252a9b68870904d77e05cd
Angular CLI <angular-cli@angular.io>
2017-07-24 17:10:22 -0300

    chore: initial commit from @angular/cli

---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class FirebaseAppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
